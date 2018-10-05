4e6af6687a7ab53003f367c443b86f6319f98dad
mentor.nakamura.tk@gmail.com <mentor.nakamura.tk@gmail.com>
2018-10-05 11:03:39 +0900

    init

---
+++ message_board_dump.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.7.23, for macos10.13 (x86_64)
+--
+-- Host: localhost    Database: message_board
+-- ------------------------------------------------------
+-- Server version	5.7.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+INSERT INTO `messages` VALUES (1,'helloああ','2018-10-02 21:35:38','taroあああ','2018-10-04 11:36:16'),(7,'s','2018-10-04 12:12:43','s','2018-10-04 12:12:43'),(3,'aaaaa','2018-10-03 18:07:05','aaa','2018-10-03 18:07:05'),(6,'あ','2018-10-04 12:12:40','あ','2018-10-04 12:12:40'),(8,'２','2018-10-04 12:12:47','２','2018-10-04 12:12:47'),(9,'2','2018-10-04 12:12:56','二','2018-10-04 12:12:56'),(10,'あ','2018-10-04 12:13:05','あ','2018-10-04 12:13:05'),(11,'あ','2018-10-04 12:13:08','あ','2018-10-04 12:13:08'),(12,'s','2018-10-04 12:13:12','s','2018-10-04 12:13:12'),(13,'３','2018-10-04 12:13:16','３','2018-10-04 12:13:16'),(14,'２','2018-10-04 12:13:21','２','2018-10-04 12:13:21'),(15,'３','2018-10-04 12:13:25','３','2018-10-04 12:13:25'),(16,'２','2018-10-04 12:13:29','２','2018-10-04 12:13:29'),(17,'１','2018-10-04 12:13:33','１','2018-10-04 12:13:33'),(18,'１','2018-10-04 12:13:37','１','2018-10-04 12:13:37');
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-04 13:02:06
